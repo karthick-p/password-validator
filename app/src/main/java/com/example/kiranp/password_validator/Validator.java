6da1ccdd963400c39eee644b71924e6ab339e6c2
karthick-p <kr688977@dal.ca>
2018-02-10 04:43:19 -0400

    a2-stage2
    a2-stage2 commit

--- app/src/main/java/com/example/kiranp/password_validator/Validator.java
+++ app/src/main/java/com/example/kiranp/password_validator/Validator.java
@@ -1,6 +1,8 @@
 package com.example.kiranp.password_validator;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Validator {
@@ -16,6 +18,21 @@ public class Validator {
         }
         if (!(text1.length() < 9)) {
             pass_rules += 1;/*rule 2 length greater than 8*/
+        }
+        Pattern special = Pattern.compile("[^a-z A-Z0-9]");
+        Matcher special_charac = special.matcher(text1);
+        if (special_charac.find()){
+            pass_rules += 1;/* 3rd rule,one special character must in password*/
+        }
+        Pattern repeat = Pattern.compile("(?=(.)\\1{3})(\\1+)");
+        Matcher repeat_charac = repeat.matcher(text1);
+        if (!(repeat_charac.find())){
+            pass_rules += 1;/* 4th rule, not more than 3 consecutive repeated letters*/
+        }
+        Pattern number = Pattern.compile("[0-9]");
+        Matcher numb_present = number.matcher(text1);
+        if (numb_present.find()){
+            pass_rules += 1;/* 5th rule,one number must in password*/
         }
         return pass_rules;
     }
--- app/src/test/java/com/example/kiranp/password_validator/Validator_tester.java
+++ app/src/test/java/com/example/kiranp/password_validator/Validator_tester.java
@@ -3,26 +3,56 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class Validator_tester {
+
     @Test
     public void test1()/*all rules pass*/
     {
         Validator test = new Validator();
-        String a = "password1";
-        Assert.assertEquals(test.validate(a), 2);
+        String a = "Km_124567";
+        Assert.assertEquals(test.validate(a), 5);
     }
     @Test
-    public void test2()/*rule 2 fail*/
+    public void test2()/*rule 1 fail*/
     {
         Validator test = new Validator();
-        String a = "Km_12456";
+        String a = "password";
         Assert.assertEquals(test.validate(a), 1);
     }
     @Test
-    public void test3()/*all fail*/
+    public void test3()/*rule 2 fail*/
     {
         Validator test = new Validator();
-        String a = "Password";
-        Assert.assertEquals(test.validate(a), 0);
+        String a = "Km_124";
+        Assert.assertEquals(test.validate(a), 4);
     }
+    @Test
+    public void test4()/*rule 3 fail*/
+    {
+        Validator test = new Validator();
+        String a = "Km124567";
+        Assert.assertEquals(test.validate(a),3);
+    }
+    @Test
+    public void test5()/*rule 4 fail*/
+    {
+        Validator test = new Validator();
+        String a = "KKKK_m124567";
+        Assert.assertEquals(test.validate(a),4);
+    }
+    @Test
+    public void test6()/*rule 5 fail*/
+    {
+        Validator test = new Validator();
+        String a = "Km_Km_Rm";
+        Assert.assertEquals(test.validate(a),3);
+    }
+    @Test
+    public void test7()/*all rule fail*/
+    {
+        Validator test = new Validator();
+        String a = "KKKK";
+        Assert.assertEquals(test.validate(a),1);
+    }
+
 }
 
